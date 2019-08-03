05c557563275643bf4b71642112331a8fbe047df
tan jun rong <tanjunrong321@gmail.com>
2019-08-03 22:02:20 +0800

    Removed collision enter because it is checked in the bullet script

--- One_In_The_Chamber/Assets/Scripts/Entity/Enemy.cs
+++ One_In_The_Chamber/Assets/Scripts/Entity/Enemy.cs
@@ -24,12 +24,4 @@ public class Enemy : Character {
 	protected void MoveTowardsPlayer() {
 		charRB.velocity = transform.up * movementSpeed * Time.deltaTime;
 	}
-
-	private void OnCollisionEnter2D(Collision2D collision) {
-		// TODO: Make enemy get hit.
-		// (For guns that dont depend on raycast.)
-
-		// Call the below function as a callback to the bullet to indiciate that it hit an enemy.
-		//collision.gameObject.GetComponent<Bullet>().TriggerBulletContactedEnemy();
-	}
 }
