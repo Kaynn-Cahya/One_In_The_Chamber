8e6b480ebabeb43784eeb30ef291bc246b57d208
Kaynn Cahya <kaynncahya@outlook.com>
2019-08-03 22:06:38 +0900

    Added SceneTransitionManager

---
+++ One_In_The_Chamber/Assets/Scripts/Managers/SceneTransitionManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.SceneManagement;
+
+public class SceneTransitionManager : Singleton<SceneTransitionManager> {
+    public void TransitionToSceneBySceneName(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void TransitionToSceneBySceneIndex(int sceneIndex) {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
