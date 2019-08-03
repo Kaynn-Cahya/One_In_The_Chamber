a407b01e7b49ace2f97d95db0b96ad2d9c52bbb4
Kaynn Cahya <kaynncahya@outlook.com>
2019-08-04 00:16:18 +0900

    Added crosshair

---
+++ One_In_The_Chamber/Assets/Scripts/Utils/SnapToMousePos.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+using MyBox;
+
+public class SnapToMousePos : MonoBehaviour {
+
+    [SerializeField, Tooltip("The camera to use"), MustBeAssigned]
+    private Camera targetCamera;
+
+    void LateUpdate() {
+        transform.position = (Vector2) targetCamera.ScreenToWorldPoint(Input.mousePosition);
+    }
+}
