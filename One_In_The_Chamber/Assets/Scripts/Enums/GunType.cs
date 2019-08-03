f6572fa2d18d07e2eb42c2692e73be5ee80ff20a
Kaynn Cahya <kaynncahya@outlook.com>
2019-08-03 14:06:48 +0900

    Added base gun and bullet script

---
+++ One_In_The_Chamber/Assets/Scripts/Enums/GunType.cs
@@ -0,0 +1,10 @@
+﻿[System.Serializable]
+public enum GunType {
+    PISTOL,
+    HEAVY_PISTOL,
+    SUBMACHINE_GUN,
+    SHOTGUN,
+    SNIPER_SEMIAUTO,
+    SNIPER_RIFLE,
+    ROCKET_LAUNCHER
+}
---
+++ One_In_The_Chamber/Assets/Scripts/Weapons/Gun.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+using MyBox;
+
+public abstract class Gun : MonoBehaviour {
+
+    [SerializeField, Tooltip("What type of gun is this."), SearchableEnum]
+    protected GunType gunType;
+
+    [SerializeField, Tooltip("What bullet prefab this gun fires"), MustBeAssigned]
+    protected Bullet bulletPrefab;
+
+    [SerializeField, Tooltip("How much recoil this gun has"), PositiveValueOnly]
+    protected float gunRecoil;
+
+    public abstract bool CanFire { get; protected set; }
+
+    /// <summary>
+    /// Fires this gun.
+    /// </summary>
+    public abstract void Fire();
+
+    /// <summary>
+    /// Loads this gun with ammunition.
+    /// </summary>
+    public abstract void LoadGun();
+}
