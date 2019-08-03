32bf24130ac1f82a2f5db50feff245c8d4484844
Kaynn Cahya <kaynncahya@outlook.com>
2019-08-03 15:58:19 +0900

    Added script to rotate any character to a given position.
    Player can now rotate towards the mouse position

---
+++ One_In_The_Chamber/Assets/Scripts/Entity/Character.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using MyBox;
+
+public abstract class Character : MonoBehaviour {
+
+    [Separator("Character Rotation Properties")]
+
+    [SerializeField, Tooltip("How fast this character can rotate around"), PositiveValueOnly]
+    private float rotationSpeed;
+
+    [SerializeField, Tooltip("The offset value to use when this character is rotating around"), Range(-360f, 360f)]
+    private float rotationOffsetValue;
+
+    /// <summary>
+    /// Rotate this character to face the desired position, limited by the deltaTime and the character's rotation speed.
+    /// </summary>
+    /// <param name="position"></param>
+    /// <param name="deltaTime"></param>
+    protected void RotateCharacterToPositionOnFrame(Vector3 position, float deltaTime) {
+        float angle = GetAngleToRotateTowards() - rotationOffsetValue;
+
+        Quaternion rotateTo = FindRotationValueInQuaternion();
+
+        RotatePlayerToRotationValue();
+
+        #region Local_Function
+
+        float GetAngleToRotateTowards() {
+            Vector2 direction = position - transform.position;
+            return Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        }
+
+        Quaternion FindRotationValueInQuaternion() {
+            return Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+
+        void RotatePlayerToRotationValue() {
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotateTo, rotationSpeed * deltaTime);
+        }
+
+        #endregion
+    }
+}
---
+++ One_In_The_Chamber/Assets/Scripts/Entity/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using MyBox;
+
+public class Player : Character {
+
+    [SerializeField, Tooltip("The camera used for the player to aim with the mouse in the game"), MustBeAssigned]
+    private Camera gameCamera;
+
+    private void Update() {
+        RotatePlayerBasedOnMousePosition(Time.deltaTime);
+    }
+
+    private void RotatePlayerBasedOnMousePosition(float deltaTime) {
+
+        Vector3 positionToRotateTowards = gameCamera.ScreenToWorldPoint(Input.mousePosition);
+
+        RotateCharacterToPositionOnFrame(positionToRotateTowards, deltaTime);
+    }
+}
